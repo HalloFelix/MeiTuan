bde60e176481f0a9b5b535df8a7f2e8b94c89365
Felix <741883227@qq.com>
2020-03-25 12:17:53 +0800

    restart the project

---
+++ components/public/header/geo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location" />北京市
+    <nuxt-link class="changCity"
+               to="/changCity">切换城市</nuxt-link>
+    [柳州 北海 南宁]
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
